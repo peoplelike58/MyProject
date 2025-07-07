9dc82a2bde2994a5ebd43996b93a77e052affeec
peoplelike58 <peoplelike58@gmail.com>
2025-07-07 21:02:48 +0800

    RWD 漢堡選單與header修正

---
+++ js/hamBurger.js
@@ -0,0 +1,13 @@
+// 漢堡選單
+$(function(){
+    $('.hamburger').click(function(){
+        $('#menu').toggleClass('active')
+    })
+})
+
+// 漢堡打叉
+$(function(){
+    $('.hamburger').click(function(){
+        $('.hamburger').toggleClass('active')
+    })
+})
